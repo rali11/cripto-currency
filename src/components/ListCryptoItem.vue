645fa886ee76b6a27e5e85eaf46fdbb16d7a398d
Edu <edu_2015@live.com>
2022-07-17 20:40:06 -0300

    add-media-querys

--- src/components/ListCryptoItem.vue
+++ src/components/ListCryptoItem.vue
@@ -112,24 +112,79 @@
 </script>
 
 <style scoped>
-  .loading {
-    background: #eee;
-    background: linear-gradient(90deg, #ececec 8%, #f5f5f5 18%, #ececec 33%);
-    background-size: 200% 100%;
-    animation: 1.5s shine linear infinite;
-    border-width: 0;
+@media (max-width: 575.98px) {
+  li {
+    display: grid;
+    grid-template-columns: 1.2fr 1fr;
+    padding: 1.3rem;  
+    margin-bottom:8px;
+    border: 2px solid #f2f3f5;
+    border-radius:10px;
+    background-color: white;       
+    transition: all .9s ease-in-out;
+    cursor: pointer;    
   }
-  .small-skeleton {
-    width: 40%;
-    height: auto;
-    margin-bottom:3px;
-    margin-top:0;
+  .logo-info {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    gap: .7rem;
   }
-  .normal-skeleton {
-    width: 80%;
-    height: auto;
-    margin-bottom:3px;
+  .logo-info figure {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0;
   }
+  .logo-info figure img {
+    height:  3rem;
+    width: 3rem;
+    padding: 5px;
+    border: 1px solid #eeeeee;  
+    border-radius: 50%; 
+  }
+  .logo-info > *:last-child{    
+    display: grid;
+    grid-template-columns: 1fr;    
+    overflow: hidden;
+  }
+  .logo-info > *:last-child > p {
+    grid-row:2/3;
+  }
+  .logo-info > *:last-child > strong {
+    font-size: 1.1rem;
+     text-overflow: ellipsis;    
+    overflow: hidden;
+    grid-row:1/2;
+  }
+  .price-section {
+    display: grid;
+    grid-template-columns: 1fr; 
+    gap: 0;
+  }
+  .price-section > *:nth-child(1){
+    display: none;
+  }
+  .price-section > *:nth-child(2){
+    display: none;
+  }
+  .price-section > *:nth-child(3){
+    font-size: 1.1rem;
+    text-align: end;
+    margin-left: auto;
+  }
+  .price-section > *:last-child{
+    margin-left: auto;
+    text-align: end;    
+    font-weight: 400;
+  }
+  .list-item-label {
+    margin:0;
+    font-weight: 400;
+    color:#95949d;
+    text-overflow: ellipsis;
+  }  
+}
+@media (min-width: 576px) {
   li {
     display: grid;
     grid-template-columns: 1fr 1fr;
@@ -178,21 +233,42 @@
     grid-template-columns: 1fr 1fr; 
     gap: 0;
   }
-  .price-section > .list-item-label:nth-child(2){
+  .price-section > *:nth-child(2){
+    margin-left: auto;    
     text-align: end;
   }
   .price-section > *:nth-child(3){
     font-size: 1.1rem;
   }
   .price-section > *:last-child{
-    font-size: 1.1rem;
+    margin-left: auto;
     text-align: end;
+    font-size: 1.1rem;
   }
   .list-item-label {
     margin:0;
     font-weight: 400;
     color:#95949d;
     text-overflow: ellipsis;
+  }  
+}
+  .loading {
+    background: #eee;
+    background: linear-gradient(90deg, #ececec 8%, #f5f5f5 18%, #ececec 33%);
+    background-size: 200% 100%;
+    animation: 1.5s shine linear infinite;
+    border-width: 0;
+  }
+  .small-skeleton {
+    width: 40%;
+    height: auto;
+    margin-bottom:3px;
+    margin-top:0;
+  }
+  .normal-skeleton {
+    width: 80%;
+    height: auto;
+    margin-bottom:3px;
   }
   .positive {
     margin:0;
